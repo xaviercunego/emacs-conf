bb2902d51650e9e206a0fae09c3e9f3f7ae3edf8
Xavier Cordonnier <xcordonnier@witekio.com>
2025-02-03 11:13:13 -0800

    Add GitLab files mode

--- init.el
+++ init.el
@@ -126,7 +126,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(markdown-mode dts-mode doom-themes doom-modeline counsel ivy use-package gnu-elpa-keyring-update)))
+   '(gitlab-ci-mode markdown-mode dts-mode doom-themes doom-modeline counsel ivy use-package gnu-elpa-keyring-update)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
