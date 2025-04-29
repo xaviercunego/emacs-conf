ceacd6b87aed8077de2eaeb00e73f7b939793784
Xavier Cordonnier <xcordonnier@witekio.com>
2025-04-28 20:36:36 -0700

    Enable highlight lines mode for magit log

--- init.el
+++ init.el
@@ -193,6 +193,8 @@
 (use-package magit
   :custom
   (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
+  (add-hook 'magit-log-mode-hook 'hl-line-mode)
+
 
 ;; CUSTOM MODES ;;
 
