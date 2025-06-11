40ef7a3cc455c0edb623f24ebfd3889989a60b37
Xavier Cordonnier <xcordonnier@witekio.com>
2025-06-11 14:21:28 -0700

    Add dockerfile mode

--- init.el
+++ init.el
@@ -195,6 +195,9 @@
   (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
   (add-hook 'magit-log-mode-hook 'hl-line-mode)
 
+(use-package dockerfile-mode
+  :mode "Dockerfile\\'")
+
 
 ;; CUSTOM MODES ;;
 
