2bd6e3d6c23f9e477aaa5eeb425fa9446ac4f02d
Xavier Cordonnier <xcordonnier@witekio.com>
2025-11-26 15:44:26 -0800

    Update Bitbake files syntax mode

--- init.el
+++ init.el
@@ -269,9 +269,8 @@
 ;; Bitbake files syntax mode
 (add-to-list 'load-path "~/.emacs.d/manual-modes/bb-mode")
 (require 'bb-mode)
-(setq auto-mode-alist (cons '("\\.bb$" . bb-mode) auto-mode-alist))
-(setq auto-mode-alist (cons '("\\.inc$" . bb-mode) auto-mode-alist))
-(setq auto-mode-alist (cons '("\\.bbappend$" . bb-mode) auto-mode-alist))
-(setq auto-mode-alist (cons '("\\.bbclass$" . bb-mode) auto-mode-alist))
-(setq auto-mode-alist (cons '("\\.conf$" . bb-mode) auto-mode-alist))
-
+(setq auto-mode-alist (cons '("\\.bb\\'" . bb-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '("\\.inc\\'" . bb-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '("\\.bbappend\\'" . bb-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '("\\.bbclass\\'" . bb-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '("\\.conf\\'" . bb-mode) auto-mode-alist))
